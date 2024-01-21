5a713392bd544c4e8dbc970c9da46959f7334ece
venom <abdo30201080101694@gmail.com>
2024-01-21 14:01:48 +0200

    finish style of the input field

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import '../css/input.css'
+import React from "react";
+import "../css/input.css";
 
 export default function Input() {
   return (
-    <div>
-      
+    <div className="input-area">
+          <input className="input" type="text" placeholder="Create a new todo..." />
+          <button className="submmit-button " ></button>
     </div>
-  )
+  );
 }
