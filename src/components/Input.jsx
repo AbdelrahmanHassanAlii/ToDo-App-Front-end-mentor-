236e8f2e40d35f2579957a71a79c991f97f29a63
unknown <abdo30201080101694@gmail.com>
2024-01-23 12:50:42 +0200

    get text from input field them save it in local storage

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/input.css";
 
-let addToLocal = () => {
-  console.log("Submitted");
-};
+
 
 export default function Input() {
+  const [newTodo, setNewTodo] = useState('');
+
+  let addToLocal = () => {
+    // console.log("Submitted");
+    let todoText = newTodo.trim();
+    // console.log(todoText);
+    if (todoText !== '') {
+      // Get the current todoList from local storage
+      const todoListString = localStorage.getItem('todoList');
+      const todoList = JSON.parse(todoListString) || [];
+
+      // Update the todoList with the new todo
+      todoList.push(todoText);
+
+      // Save the updated todoList to local storage
+      localStorage.setItem('todoList', JSON.stringify(todoList));
+
+      // Clear the input field
+      setNewTodo('');
+
+      console.log(todoList);
+
+    }
+  };
+
   return (
     <div className="input-area">
-      <input className="input" type="text" placeholder="Create a new todo..." />
+      <input className="input" type="text" placeholder="Create a new todo..." value={newTodo} onChange={(e)=>setNewTodo(e.target.value)}/>
       <button className="submmit-button" onClick={addToLocal}></button>
     </div>
   );
