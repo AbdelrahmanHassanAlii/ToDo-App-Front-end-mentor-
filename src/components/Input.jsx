ee806810904df43c082f9f98644b51e17f54fe13
venom <abdo30201080101694@gmail.com>
2024-01-23 09:48:07 +0200

    on click on the button print submmited on the console

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "../css/input.css";
 
+let addToLocal = () => {
+  console.log("Submitted");
+};
+
 export default function Input() {
   return (
     <div className="input-area">
-          <input className="input" type="text" placeholder="Create a new todo..." />
-          <button className="submmit-button " ></button>
+      <input className="input" type="text" placeholder="Create a new todo..." />
+      <button className="submmit-button" onClick={addToLocal}></button>
     </div>
   );
 }
