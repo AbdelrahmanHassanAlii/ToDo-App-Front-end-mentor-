75af850f468fc60f5be4efcf84adef45eca783d7
venom <abdo30201080101694@gmail.com>
2024-01-23 23:40:48 +0200

    handle the style of the button onClick

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "../css/todoList.css";
-import "../js/todoList";
 
 export default function TodoList() {
   const [todoList, setTodoList] = useState([]);
+
   useEffect(() => {
     const storedTodoList = localStorage.getItem("todoList");
     if (storedTodoList) {
@@ -13,14 +13,47 @@ export default function TodoList() {
     }
   }, [todoList]);
 
+  const handleButtonClick = (e) => {
+    const updatedTodoList = [...todoList];
+    let index = parseInt(e.target.id);
+
+    // Check if the index is valid
+    if (index >= 0 && index < updatedTodoList.length) {
+      updatedTodoList[index].status =
+        updatedTodoList[index].status === "pending" ? "completed" : "pending";
+
+      localStorage.setItem("todoList", JSON.stringify(updatedTodoList));
+      setTodoList(updatedTodoList);
+    }
+  };
+
   return (
     <div className="list">
       <ul>
         {todoList.map((todo, index) => (
           <div className="todo-card">
-            <button className="button selected"></button>
+            <button
+              id={index}
+              className={`button ${
+                todo.status === "completed" ? "finished" : ""
+              }`}
+              onClick={handleButtonClick}
+            >
+              {todo.status === "completed" ? (
+                <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9">
+                  <path
+                    fill="none"
+                    stroke="#FFF"
+                    stroke-width="2"
+                    d="M1 4.304L3.696 7l6-6"
+                  />
+                </svg>
+              ) : (
+                ""
+              )}
+            </button>
             <li key={index} className="todo">
-              {todo}
+              {todo.text}
             </li>
           </div>
         ))}
